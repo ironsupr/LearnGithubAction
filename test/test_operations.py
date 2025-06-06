e0a44632f58048fdc6393e9630ca93ba82f0b3bb
ironsupr <abhinavsingh25001@gmail.com>
2025-06-06 09:46:51 +0530

    unit test cases updated

---
+++ test/test_operations.py
@@ -0,0 +1,12 @@
+from src.math_operations import add, subtract
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(5, 3) == 8
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(10, 5) == 5
